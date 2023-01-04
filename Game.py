055e4535cb9521b1fc16ad55bfbdd68ceff2eb7b
Hakimov Bulat <bulathakimov1704@gmail.com>
2023-01-04 14:45:58 +0300

    Second day

--- Game.py
+++ Game.py
@@ -80,3 +80,67 @@ def level_screen(level):
     
 
 level_screen('first_level.png')
+
+
+if __name__ == '__main__':
+    pygame.init()
+    pygame.display.set_caption('Уровень 1')
+    size = WIDTH, HEIGHT
+    screen = pygame.display.set_mode(size)
+
+    running = True
+    ball_pos = 300, 250
+    platform_pos = 225, 450, 100, 10
+    v = 100
+    fps = 150
+    clock = pygame.time.Clock()
+    move = 'left_up'
+    while running:
+        screen.fill((0, 0, 0))
+        pygame.draw.rect(screen, (255, 0, 0), platform_pos, 0)
+        pygame.draw.circle(screen, 'white', ball_pos, 10)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    platform_pos = platform_pos[0] + 40, 450, 100, 10
+                if event.key == pygame.K_LEFT:
+                    platform_pos = platform_pos[0] - 40, 450, 100, 10
+                    
+        #v += 1
+        
+
+        if move == 'left_up':
+            ball_pos = ball_pos[0] - v / fps, ball_pos[1] - v / fps
+        elif move == 'right_up':
+            ball_pos = ball_pos[0] + v / fps, ball_pos[1] - v / fps
+        elif move == 'left_down':
+            ball_pos = ball_pos[0] - v / fps, ball_pos[1] + v / fps
+        elif move == 'right_down':
+            ball_pos = ball_pos[0] + v / fps, ball_pos[1] + v / fps
+        pygame.draw.circle(screen, 'white', ball_pos, 10)
+
+        if ball_pos[0] <= 10:
+            if move == 'left_up':
+                move = 'right_up'
+            else:
+                move = 'right_down'
+        elif ball_pos[0] >= size[0] - 10:
+            if move == 'right_up':
+                move = 'left_up'
+            else:
+                move = 'left_down'
+        elif ball_pos[1] <= 10:
+            if move == 'right_up':
+                move = 'right_down'
+            else:
+                move = 'left_down'
+        elif ball_pos[1] >= size[1] - 10:
+            if move == 'left_down':
+                move = 'left_up'
+            else:
+                move = 'right_up'
+        clock.tick(fps)
+        pygame.display.flip()
+    pygame.quit()
